99b88bfa59c8d720cd8f3654eb70865bc92e00a8
arromeo <adam.r.romeo@gmail.com>
2018-12-14 19:22:01 -0800

    Sets up initial server

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,11 +21,23 @@ export default class HomeScreen extends React.Component {
     title: 'NuBrew',
   };
 
+  componentDidMount() {
+    
+    fetch('http://10.0.2.2:5000/api/test')
+      .then(res => res.json())
+      .then(edit => this.setState ({
+        editThisLater: edit.result
+      }))
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container}>
-
+          <Text>-->{this.state.editThisLater}</Text>
           <View style={styles.contentContainer}>
             <View style={[styles.eventDetailsContainer, styles.homeScreenFilename]}>
               <Text>Event Header</Text>
