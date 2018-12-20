13c26f93be4588fac7e049b5f72f84d09a2e82c3
arromeo <adam.r.romeo@gmail.com>
2018-12-20 15:53:59 -0800

    Adds style to brewery search results

--- screens/search/BrewerySearch.js
+++ screens/search/BrewerySearch.js
@@ -1,30 +1,78 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, FlatList } from 'react-native';
+import { ScrollView, StyleSheet, Image, TouchableOpacity, View, Text, FlatList } from 'react-native';
 
 export default class BrewerySearch extends React.Component {
   render() {
-    const styles = this.props.styles;
     return (
-        <FlatList
-        data={this.props.data}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => 
-          <TouchableOpacity style={styles.listItemContainer}
-            onPress={() => {
-              this.props.navigate('Detail', {
-                category: "Brewery",
-                id: item.id,
-              })}
-            }
-          >
-            <View style={styles.searchResultContainer}>
-              <Text>{item.name}</Text>
-              <Text>{item.description}</Text>
-              <Text>{item.street_address}, {item.city}, {item.province}, {item.postal_code}</Text>
+        <ScrollView style={styles.container}>
+            <View>
+                <FlatList
+                  data={this.props.data}
+                  keyExtractor={item => item.id.toString()}
+                  renderItem={({item}) => 
+                <TouchableOpacity
+                  style={[styles.eventContainer, styles.homeScreenFilename]}
+                  onPress={() => {
+                    navigate('Detail', {
+                      id: item.id,
+                      category: "Event"
+                    });
+                  this.props.searchDatabase(item.id.toString(), "HighlightEvent");
+                }}>
+                <View style={[styles.eventDetailsContainer, styles.homeScreenFilename]}>
+                <View>
+                  <Image
+                    style={{width: 75, height: 75}}
+                    source={{uri: item.img_url }}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.eventTitle}>{item.name}</Text>
+                  <Text style={styles.eventDetails}>{item.street_address}</Text>
+                  <Text style={styles.eventDetails}>{item.city}, {item.province}</Text>
+                  <Text style={styles.eventDetails}>{item.postal_code}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          }/>
             </View>
-          </TouchableOpacity>
-        }
-        />
+        </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  headerFont: {
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    paddingBottom: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: 'flex-start',
+  },
+  eventDetailsContainer: {
+    flexDirection: "row",
+    flex: 1,
+    margin: 5,
+  },
+  eventDetails: {
+    marginLeft: 7,
+    justifyContent: "center",
+    fontSize: 11
+  },
+  eventTitle: {
+    marginLeft: 5,
+    fontWeight: "bold"
+  }
+});
