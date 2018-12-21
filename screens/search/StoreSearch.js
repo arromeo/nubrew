84de1c10b93ad65ccd1f6d16bcd9333081c405b8
arromeo <adam.r.romeo@gmail.com>
2018-12-20 17:00:17 -0800

    Fixes select for beer details pages

--- screens/search/StoreSearch.js
+++ screens/search/StoreSearch.js
@@ -13,11 +13,10 @@ export default class EventSearch extends React.Component {
                 <TouchableOpacity
                   style={[styles.eventContainer, styles.homeScreenFilename]}
                   onPress={() => {
-                    navigate('Detail', {
+                    this.props.navigate('Detail', {
                       id: item.id,
-                      category: "Event"
+                      category: "Store"
                     });
-                  this.props.searchDatabase(item.id.toString(), "HighlightEvent");
                 }}>
                 <View style={[styles.eventDetailsContainer, styles.homeScreenFilename]}>
                 <View>
