3276a4cc14d30aedabe0dd68d32373bdc1765415
arromeo <adam.r.romeo@gmail.com>
2018-12-27 16:03:36 -0800

    Gets rid of duplicate code in App.js

--- App.js
+++ App.js
@@ -17,15 +17,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    let url = `${port.DEV_PORT}/api/user/${this.state.user_id}/favorites`;
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        this.setState ({ favorites: data.result, })
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    this.updateFavorites();
   }
 
   // Updates the favorites list when beers are added or removed by clicking
